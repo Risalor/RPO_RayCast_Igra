dcde78a2f79ffdd839ad5a6e084ffcc970dcf09c
KristjanLorbek <84332438+Risalor@users.noreply.github.com>
2023-12-04 14:14:30 +0100

    Dodal ozadje in sound

--- RPO_Projekt/MainMenuState.cpp
+++ RPO_Projekt/MainMenuState.cpp
@@ -1,8 +1,19 @@
 #include "MainMenuState.h"
 
 void MainMenuState::initState() {
-	buttons.push_back(Button(sf::Vector2f(screenWidth / 2 + 30, 50.f), "Start"));
-	buttons.push_back(Button(sf::Vector2f(screenWidth / 2 + 30, 150.f), "Quit"));
+	buttons.push_back(Button(sf::Vector2f(150.f, 150.f), "Start"));
+	buttons.push_back(Button(sf::Vector2f(150.f, 250.f), "Quit"));
+
+	if (!buffer.loadFromFile("Assets/Music/MainMenu.wav")) {
+		std::cout << "Cannot load music\n";
+	}
+
+	menuMusic.setBuffer(buffer);
+	menuMusic.play();
+
+	background.setSize(sf::Vector2f(screenWidth + 411.42f, screenHeight));
+	tex.loadFromFile("Assets/Backgrounds/MenuBackground.png");
+	background.setTexture(&tex);
 }
 
 MainMenuState::MainMenuState() : State() {
@@ -16,17 +27,29 @@ MainMenuState::~MainMenuState() {
 void MainMenuState::update(float dt, sf::Vector2f mousePos) {
 	buttons[0].update(mousePos);
 	buttons[1].update(mousePos);
+
+	if (menuMusic.getStatus() == sf::Sound::Status::Stopped) {
+		if (!buffer.loadFromFile("Assets/Music/MainMenu.wav")) {
+			std::cout << "Cannot load music\n";
+		}
+
+		menuMusic.setBuffer(buffer);
+		menuMusic.play();
+	}
 	
 	if (buttons[0].clicked()) {
 		State::trigger = StateTrigger::START_GAME;
+		menuMusic.stop();
 	}
 
 	if (buttons[1].clicked()) {
 		State::trigger = StateTrigger::END_GAME;
+		menuMusic.stop();
 	}
 }
 
 void MainMenuState::draw(sf::RenderTarget* window) {
+	window->draw(background);
 	buttons[0].draw(window);
 	buttons[1].draw(window);
 }
